885e3efad8be2c7548ed1d6b4f7d92d6cb6f5aa3
Jack Burrus <jhburrus3@gmail.com>
2020-01-04 13:42:07 -0500

    Terminal working

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+import "../styles/styles.css"
+
+class HomeScreen extends Component {
+  render() {
+    return <div>Hello Jacks Portfolio</div>
+  }
+}
+
+export default HomeScreen
