2368720b4c0406a008344c27445aa21d218511e6
Jack <jhburrus3@gmail.com>
2020-07-01 21:14:45 -0400

    New design looking good

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,79 +1,101 @@
-import '../styles/styles.css'
+import "../styles/styles.css"
 
-import React from 'react'
+import React from "react"
 
-import Underline from '../../assets/headerUnderline.svg'
+import Tilt from "react-tilt"
+import Underline from "../../assets/headerUnderline.svg"
 
 // icons
-import Github from '../../assets/github.svg'
-import LinkedIn from '../../assets/linkedin.svg'
-import AWS from '../../assets/icons/amazonaws.svg'
-import Python from '../../assets/icons/python.svg'
-import Serverless from '../../assets/icons/serverless.svg'
-import JavaScript from '../../assets/icons/javascript.svg'
-import BackgroundMountains from '../../assets/backgroundmtns.svg'
-import ReactIcon from '../../assets/icons/react.svg'
-import Expo from '../../assets/icons/Expo.svg'
-import WorksRow from '../components/WorksRow'
-import MobileWorks from '../components/MobileWorks'
+import Github from "../../assets/github.svg"
+import LinkedIn from "../../assets/linkedin.svg"
+import AWS from "../../assets/icons/amazonaws.svg"
+import Python from "../../assets/icons/python.svg"
+import Serverless from "../../assets/icons/serverless.svg"
+import JavaScript from "../../assets/icons/javascript.svg"
+import BackgroundMountains from "../../assets/backgroundmtns.svg"
+import ReactIcon from "../../assets/icons/react.svg"
+import Expo from "../../assets/icons/Expo.svg"
+import WorksRow from "../components/WorksRow"
+import MobileWorks from "../components/MobileWorks"
 
-import useViewport from '../../static/hooks/useViewport.js'
+import useViewport from "../../static/hooks/useViewport.js"
+import Sana from "../../assets/SanaSmall.svg"
+import Me from "../../assets/MeAsWellAlso.svg"
 
 const App = () => {
   const { width } = useViewport()
   const breakpoint = 620
   return (
     <div className="container">
-      <div className="headerStyles">
-        <div className="nameAndIconsContainer">
-          <div className="nameAndTitle">
-            <div className="name">Jack Burrus</div>
-            <div className="title">Mobile and Web Developer</div>
-          </div>
-          <div className="iconContainer">
-            <a href="https://github.com/jackburrus">
-              <Github />
-            </a>
-            <a href="https://www.linkedin.com/in/jamesburrus/">
-              <LinkedIn />
-            </a>
-          </div>
-        </div>
+      <Me />
+      <div className="projectContainer">
+        <Tilt
+          className="Tilt"
+          options={{ max: 25 }}
+          style={{
+            height: 300,
+            width: 300,
+            margin: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Sana />
+        </Tilt>
+        <Tilt
+          className="Tilt"
+          options={{ max: 25 }}
+          style={{
+            height: 300,
+            width: 300,
+            margin: 20,
 
-        <Underline className="headerUnderline" />
-      </div>
-      {/* {width > breakpoint ? ( */}
-      <div className="worksContainer">
-        <div className="column">
-          <WorksRow
-            name="OSINT Gadget"
-            description="Chrome extension for OSINT analysts"
-            rowStyles="rowStyles"
-            icons={[<AWS />, <JavaScript />, <Python />, <Serverless />]}
-            link="https://www.github.com/jackburrus"
-          />
-          <WorksRow
-            name="Dollop"
-            description="Recipe app"
-            rowStyles="rowStyles"
-            icons={[<JavaScript />, <Expo />]}
-            link="https://www.github.com/jackburrus"
-          />
-          <WorksRow
-            name="Coinbase Clone"
-            description="A Coinbase clone made entirely in React Native"
-            rowStyles="rowStyles"
-            icons={[<JavaScript />, <Expo />]}
-            link="https://github.com/jackburrus/coinbaseClone"
-          />
-        </div>
-      </div>
-      {/* ) : ( */}
-      {/* <MobileWorks />
-      )} */}
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Sana />
+        </Tilt>
+        <Tilt
+          className="Tilt"
+          options={{ max: 25 }}
+          style={{
+            height: 300,
+            width: 300,
+            margin: 20,
 
-      <div className="mountainsContainer">
-        <BackgroundMountains className="backgroundMountains" />
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Sana />
+        </Tilt>
+        <Tilt
+          className="Tilt"
+          options={{ max: 25 }}
+          style={{
+            height: 300,
+            width: 300,
+
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Sana />
+        </Tilt>
+        <Tilt
+          className="Tilt"
+          options={{ max: 25 }}
+          style={{
+            height: 300,
+            width: 300,
+
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Sana />
+        </Tilt>
       </div>
     </div>
   )
