5a17989fcfbb80c7028616b5c6ba1501650cc69c
Jack Burrus <jhburrus3@gmail.com>
2020-07-06 12:12:59 -0400

    slight style changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,16 @@ import "../styles/styles.css"
 import React from "react"
 
 import Tilt from "react-tilt"
-import Underline from "../../assets/headerUnderline.svg"
-
-// icons
-import Github from "../../assets/github.svg"
-import LinkedIn from "../../assets/linkedin.svg"
-import AWS from "../../assets/icons/amazonaws.svg"
-import Python from "../../assets/icons/python.svg"
-import Serverless from "../../assets/icons/serverless.svg"
-import JavaScript from "../../assets/icons/javascript.svg"
-import BackgroundMountains from "../../assets/backgroundmtns.svg"
-import ReactIcon from "../../assets/icons/react.svg"
-import Expo from "../../assets/icons/Expo.svg"
-import WorksRow from "../components/WorksRow"
-import MobileWorks from "../components/MobileWorks"
 
 import useViewport from "../../static/hooks/useViewport.js"
 import Sana from "../../assets/SanaSmall.svg"
-import Me from "../../assets/MeAsWellAlso.svg"
+import Me from "../../assets/Sana.svg"
 
 const App = () => {
   const { width } = useViewport()
   const breakpoint = 620
   return (
     <div className="container">
-      <Me />
       <div className="projectContainer">
         <Tilt
           className="Tilt"
@@ -40,61 +25,7 @@ const App = () => {
             alignItems: "center",
           }}
         >
-          <Sana />
-        </Tilt>
-        <Tilt
-          className="Tilt"
-          options={{ max: 25 }}
-          style={{
-            height: 300,
-            width: 300,
-            margin: 20,
-
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Sana />
-        </Tilt>
-        <Tilt
-          className="Tilt"
-          options={{ max: 25 }}
-          style={{
-            height: 300,
-            width: 300,
-            margin: 20,
-
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Sana />
-        </Tilt>
-        <Tilt
-          className="Tilt"
-          options={{ max: 25 }}
-          style={{
-            height: 300,
-            width: 300,
-
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Sana />
-        </Tilt>
-        <Tilt
-          className="Tilt"
-          options={{ max: 25 }}
-          style={{
-            height: 300,
-            width: 300,
-
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Sana />
+          <Me className="item" />
         </Tilt>
       </div>
     </div>
